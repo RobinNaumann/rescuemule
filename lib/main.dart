7929d916eb196cfa80621a600463e06b75a0c9c5
Robin Naumann <constorux@gmail.com>
2025-05-17 10:57:45 +0200

    imp: reorganized ui files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:elbe/elbe.dart';
 import 'package:rescuemule/service/s_bluetooth.dart';
+import 'package:rescuemule/view/vp_home.dart';
 
 const appName = "RescueMule";
 
@@ -11,7 +12,7 @@ void main() async {
 }
 
 final router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, _) => const YourPage())],
+  routes: [GoRoute(path: '/', builder: (context, _) => const HomePage())],
 );
 
 class YourApp extends StatelessWidget {
@@ -19,47 +20,4 @@ class YourApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ElbeApp(router: router);
-}
-
-class YourPage extends StatelessWidget {
-  const YourPage({super.key});
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    title: appName,
-    child: Padded.all(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-
-        children:
-            [
-              Button.minor(
-                label: "advertise",
-                onTap: () async {
-                  context.showToast("TODO: advertise");
-                  //await advertise();
-                },
-              ),
-              Button.minor(
-                label: "scan",
-                onTap: () async {
-                  context.showToast("TODO: discover");
-                  //await discover();
-                },
-              ),
-              Mule(name: "Jonas"),
-            ].spaced(),
-      ),
-    ),
-  );
-}
-
-class Mule extends StatelessWidget {
-  final String name;
-  const Mule({super.key, this.name = "Unknown Mule"});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text("Hello I'm the mule $name");
-  }
 }
---
+++ lib/view/vp_home.dart
@@ -0,0 +1,45 @@
+import 'package:elbe/elbe.dart';
+import 'package:rescuemule/main.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    title: appName,
+    child: Padded.all(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+
+        children:
+            [
+              Button.minor(
+                label: "advertise",
+                onTap: () async {
+                  context.showToast("TODO: advertise");
+                  //await advertise();
+                },
+              ),
+              Button.minor(
+                label: "scan",
+                onTap: () async {
+                  context.showToast("TODO: discover");
+                  //await discover();
+                },
+              ),
+              Mule(name: "Jonas"),
+            ].spaced(),
+      ),
+    ),
+  );
+}
+
+class Mule extends StatelessWidget {
+  final String name;
+  const Mule({super.key, this.name = "Unknown Mule"});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello I'm the mule $name");
+  }
+}
